408236b345775af481538e08ba32021a263eabeb
Pavan M <pavanmgowda20@gmail.com>
2025-11-25 16:43:41 +0530

    Create print.java with variable examples
    Added a Java program that prints a message and demonstrates variable types.

---
+++ print.java
@@ -0,0 +1,23 @@
+public class print {
+     public static void main(String[] args) {
+        // Print a message to the console
+        System.out.println("Hello,Java!");
+        // variables
+        String name = "Aman";
+        String neighbour = "Pavan";
+        int age = 22;
+        // java types
+        // primitive types
+        byte ageg = 30;
+        int phone = 1234567890;
+        long phone2 = 12345678900L;
+        float pi = 3.14F;
+        char letter = 'n';
+        boolean isAdult = true;
+        // non primitive types
+        String hesaru = "Pavan";
+        String hesary = new String("Pavana");
+        String hesare =new String("Adaman");
+        System.out.println(hesaru.length());
+    }
+}
