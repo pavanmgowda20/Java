97c3b18b7b9bec19f0c9905419eb5e1e2463ac8b
Pavan M <pavanmgowda20@gmail.com>
2025-11-27 00:15:13 +0530

    Implement name input functionality
    Added functionality to input and display a name.

---
+++ input.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Input {
+    public static void main(String[] args){
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Input Your Age: ");
+        // int age= sc.nextInt();
+        // System.out.println(age);
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Input Your Age: ");
+        // Float age= sc.nextFloat();
+        // System.out.println(age);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input Your Name: ");
+        String name= sc.next();
+        System.out.println(name);
+    }
+}
