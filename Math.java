05ee60d6f21aa1f1700ab9fa08d297d1db7cffa2
Pavan M <pavanmgowda20@gmail.com>
2025-12-17 20:28:16 +0530

    Add Maths class with basic math operations

---
+++ Math.java
@@ -0,0 +1,8 @@
+public class Maths {
+    public static void main(String[] args){
+        //to get maximum of two numbers
+        System.out.println(Math.max(5,6));
+        System.out.println(Math.min(1,6));
+        System.out.println(Math.random());
+    }
+}
