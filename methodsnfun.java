2898f857da112e4466cdf0105e66e2e943382b63
Pavan M <pavanmgowda20@gmail.com>
2025-12-18 23:00:54 +0530

    Add Methodsnfuns class with various print methods
    Decompiled Java class with methods for printing and summing.

---
+++ methodsnfun.java
@@ -0,0 +1,27 @@
+// Source code is decompiled from a .class file using FernFlower decompiler (from Intellij IDEA).
+public class Methodsnfuns {
+   public Methodsnfuns() {
+   }
+
+   public static void printjava() {
+      System.out.println("Hello java");
+   }
+
+   public static void printName(String var0) {
+      System.out.println(var0);
+   }
+
+   public static void printSum(int var0, int var1) {
+      int var2 = var0 + var1;
+      System.out.println(var2);
+   }
+
+   public static void main(String[] var0) {
+      printName("Aman");
+      printName("Axxn");
+      printSum(1, 6);
+      printjava();
+      printjava();
+      printjava();
+   }
+}
