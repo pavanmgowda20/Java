e825b2a6cf2ca121e6a47fe7c1ddca82d7021dbe
Pavan M <pavanmgowda20@gmail.com>
2025-12-20 23:22:49 +0530

    Add string operations in Strings class
    Implement string operations including concatenation, charAt, replace, and substring.

---
+++ strings.java
@@ -0,0 +1,17 @@
+public class Strings {
+    public static void main(String[] args){
+    //concatenate
+    String name1 = "Pavan";
+    String  name2 = "Dhanu";
+    String name3 = name1+" and "+name2;
+    System.out.println(name3);
+    // charAt
+    String name = "Aman";
+    System.out.println(name.charAt(0));
+    String name12 = name.replace('a', 'b');
+    System.out.println(name12);
+    // substring
+    String name32 = " Aman and Akku";
+    System.out.println(name.substring(5,9));
+    }
+}
